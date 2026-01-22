136bfafd5b7fee2af5db80361eac6ea271792d52
Evelyn Barragan <barragan2020eb@outlook.com>
2026-01-21 20:43:56 -0800

    pylint

--- problem_4_11_2.py
+++ problem_4_11_2.py
@@ -20,10 +20,26 @@ leo.color("blue")
 leo.pensize(6)
 
 # 4.11.2. Exercise
-# Write a function called rhombus that draws a rhombus with a given side length and a given interior angle. 
+# Write a function called rhombus that draws a rhombus with a given side length
+# and a given interior angle.
 # For example, here’s a rhombus with side length 50 and an interior angle of 60 degrees.
 
 def rhombus(length, angle):
+    """
+    Draws a rhombus with the given length and angle
+
+    Parameters
+    ----------
+    length : int or float
+        The length of the rhombus.
+    angle : int or float
+        The angle of the rhombus.
+
+    Returns
+    -------
+    None.
+
+    """
     interior_angle = 180 - angle
     for _ in range(2):
         leo.forward(length)
