6ebef6ac10d02ec5912390bb37c76cc02a8b8d9f
Evelyn Barragan <barragan2020eb@outlook.com>
2026-01-21 20:28:06 -0800

    import turtle and create object

--- problem_4_11_2.py
+++ problem_4_11_2.py
@@ -6,3 +6,15 @@ Created on Wed Jan 21 20:25:13 2026
 @author: evebarr20
 """
 
+# import the turtle library
+import turtle
+
+# create a turtle screen, with a yellow background
+SCREEN = turtle.Screen()
+SCREEN.bgcolor("lightyellow")
+
+# turtle is name Leo(TMNT)
+
+leo = turtle.Turtle()
+leo.color("blue")
+leo.pensize(6)
