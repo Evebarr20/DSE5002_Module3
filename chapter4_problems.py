03830a7b411040fe0fb9ba6521346d47c033e581
Evelyn Barragan <barragan2020eb@outlook.com>
2026-01-21 19:53:53 -0800

    import turtle library and create turtle object

--- chapter4_problems.py
+++ chapter4_problems.py
@@ -5,4 +5,19 @@ Created on Wed Jan 21 19:45:13 2026
 
 @author: evebarr20
 """
+# import the turtle library
+import turtle
 
+# create a turtle screen, with a yellow background
+wn = turtle.screen()
+wn.bgcolor("lightyellow")
+
+# turtle is name Ralph(TMNT)
+
+ralph = turtle.Turtle()
+ralph.color("red")
+ralph.pensize(6)
+
+# 4.11.1. Exercise
+# Write a function called rectangle that draws a rectangle with given side lengths. 
+# For example, here’s a rectangle that’s 80 units wide and 40 units tall.
