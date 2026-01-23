ad4f8dada215e4184c21929ed3c0219ec64a4dbe
Evelyn Barragan <barragan2020eb@outlook.com>
2026-01-22 19:13:25 -0800

    pylint

--- problem_4_11_3.py
+++ problem_4_11_3.py
@@ -20,6 +20,23 @@ don.color("purple")
 don.pensize(6)
 
 def parallelogram(bottom, slanted, angle = 90):
+    """
+    Draws a parallelogram using a turtle.
+
+    Parameters
+    ----------
+    bottom : int or float
+        The length of the bottom and top sides.
+    slanted : int or float
+        The length of the slanted sides.
+    angle : int or float, optional
+        The interior angle between the bottom and slanted side. The default is 90.
+
+    Returns
+    -------
+    None.
+
+    """
     for _ in range(2):
         don.forward(bottom)
         don.left(angle)
@@ -27,9 +44,39 @@ def parallelogram(bottom, slanted, angle = 90):
         don.left(180 - angle)
 
 def rectangle(wide, tall):
+    """
+    Draws a rectangle  using the parallelogram function.
+    
+    Parameters
+    ----------
+    wide : int or float
+        The width of the rectangle.
+    tall : int or float
+        The height of the rectangle.
+
+    Returns
+    -------
+    None.
+
+    """
     parallelogram(wide, tall)
 
 def rhombus(length, angle):
+    """
+    Draws a rhombus using the parallelogram function.
+
+    Parameters
+    ----------
+    length : int or float
+        The length of each side of the rhombus.
+    angle : int or float
+        The interior angle of the rhombus.
+
+    Returns
+    -------
+    None.
+
+    """
     parallelogram(length, length, angle)
 
 rectangle(80, 40)
