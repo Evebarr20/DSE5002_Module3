41d489661ee238bc3a6ea2d3acf7c4030c7c9f2b
Evelyn Barragan <barragan2020eb@outlook.com>
2026-01-22 19:00:52 -0800

    Create function parallelogram

--- problem_4_11_3.py
+++ problem_4_11_3.py
@@ -6,3 +6,23 @@ Created on Wed Jan 21 20:57:27 2026
 @author: evebarr20
 """
 
+# import the turtle library
+import turtle
+
+# create a turtle screen, with a yellow background
+SCREEN = turtle.Screen()
+SCREEN.bgcolor("lightyellow")
+
+# turtle is name Mikey (TMNT)
+
+mikey = turtle.Turtle()
+mikey.color("yellow")
+mikey.pensize(6)
+
+def parallelogram(bottom, slanted, angle):
+    for _ in range(4):
+        mikey.forward(bottom)
+        mikey.left(angle)
+        mikey.forward(slanted)
+        mikey.left(180 - angle)
+    
